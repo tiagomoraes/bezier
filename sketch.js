5f1bd0fca99a488876e926b23bfb9b920640c925
Tiago Moraes <tiago3902@gmail.com>
2019-11-18 00:16:36 -0300

    Added bootstrap add createCurve code

--- sketch.js
+++ sketch.js
@@ -1,4 +1,26 @@
+// ================ CONSTANTS ================
+
+const AUTHORS = 'Tiago Moraes & Vinícius Dantas';
+
+// ================ GENERAL FUNCTIONS ================
+
+function paintCurve(points) {
+  for(let i = 1; i < points.length; i++) {
+    line(points[i].x, points[i].y, points[i-1].x, points[i-1].y);
+  }
+}
+
+// add more functions here
+
+// ================ SETUP & DRAW ================
+
 function setup() {
-  createCanvas(1200, 800);
-  background(240);
+  // add setup code before the createCanvas()
+
+
+  createCanvas(1920, 1080);
+}
+
+function draw() {
+  paintCurve(testCurve);
 }
